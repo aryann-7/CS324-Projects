25020a189de4aa8386ea92dfed5f1a8a7ed7bbf7
Your Name <aryanzsingh444@gmail.com>
2026-07-28 07:10:35 +1200

    add: source files

---
+++ Lab 1 Classes/music/Music.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package music;
+
+/**
+ *
+ * @author sharma_au
+ */
+public class Music {
+    public static void tune(Instrument i) {
+        // ...
+		//play note MIDDLE_C
+		//play note C_SHARP
+    }
+    public static void main(String[] args){
+		//create objects of Flute and Guitar
+		//call tune method
+    
+        System.out.println("End of program.");
+  }
+}
